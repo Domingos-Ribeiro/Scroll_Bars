0f19bc6e494e9021e4d7a70ed7ca9b5f4361c2a3
Domingos-Ribeiro <domingos.ribeiro.net@gmail.com>
2021-01-06 08:55:06 +0000

    Verification

--- Bars.Designer.cs
+++ Bars.Designer.cs
@@ -32,6 +32,7 @@ namespace Scroll_Bars
             this.BarraHorizTeste = new System.Windows.Forms.HScrollBar();
             this.LblTeste = new System.Windows.Forms.Label();
             this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -61,11 +62,21 @@ namespace Scroll_Bars
             this.numericUpDown1.Size = new System.Drawing.Size(42, 20);
             this.numericUpDown1.TabIndex = 2;
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(452, 351);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Bars
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(623, 484);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.numericUpDown1);
             this.Controls.Add(this.LblTeste);
             this.Controls.Add(this.BarraHorizTeste);
@@ -83,6 +94,7 @@ namespace Scroll_Bars
         private System.Windows.Forms.HScrollBar BarraHorizTeste;
         private System.Windows.Forms.Label LblTeste;
         private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
